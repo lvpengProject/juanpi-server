aa75e96c505f0bdb8296d535a07d1e093a7993ac
LPDorr <108051783+LPDorr@users.noreply.github.com>
2022-09-26 14:55:00 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <keep-alive include="List">
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style lang="stylus">
+
+</style>
